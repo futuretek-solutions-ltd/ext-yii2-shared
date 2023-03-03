c4b6845885f79e60d69b9b28d6201e46cf6f8afa
Lukáš Černý <lukas.cerny@futuretek.cz>
2023-03-03 11:28:31 +0100

    Added Date::compare()

--- Date.php
+++ Date.php
@@ -25,4 +25,23 @@ class Date extends \DateTime
 
         return new self($dateTime);
     }
+
+    /**
+     * Compare two dates.
+     * Return:
+     * <ul>
+     * <li>-1 if first <i>d1</i> is less than <i>d2</i></li>
+     * <li>1 if <i>d1</i> is greater than <i>d2</i></li>
+     * <li>0 if they are equal</li>
+     * </ul>
+     *
+     * @param \DateTime|int|string $d1 Date in valid format or DateTime object or timestamp(int)
+     * @param \DateTime|int|string $d2 Date in valid format or DateTime object or timestamp(int)
+     * @return int
+     * @throws \Exception
+     */
+    public static function compare($d1, $d2): int
+    {
+        return strcmp(self::c($d1)->format('Y-m-d'), self::c($d2)->format('Y-m-d'));
+    }
 }
